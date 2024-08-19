1a1830e6b580dc8e5e265601e0ac23e6fed2b994
Sevinda Perera <sevindacharukaperera@gmail.com>
2024-08-19 11:48:44 +0530

    feat: created shirt and camera rig

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
 
-const CameraRig = () => {
-  return (
-    <div>CameraRig</div>
-  )
-}
+import state from "../store";
 
-export default CameraRig
+const CameraRig = ({ children }) => {
+  const snap = useSnapshot(state);
+  const group = useRef();
+
+  useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    // SETTING THE INITIAL POSITION OF THE CAMERA
+    let targetPosition = [-0.4, 0, 2];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0, 2.5];
+    } else {
+      if (isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
+    }
+
+    // SETTING THE MODEL CAMERA POSITION
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
+    // SETTING THE MODEL ROTATION SMOOTHLY
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
+
+  return <group ref={group}>{children}</group>;
+};
+
+export default CameraRig;
